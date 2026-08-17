e243398c17f9678da63c05ef00e506e157bc68a8
phonggioan <tlpgioan210707@gmail.com>
2026-08-17 15:32:49 +0700

    updated3

---
+++ baitapsession02/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("C1");
+        Console.WriteLine("Enter x");
+        float x = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter y:");
+        float y = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Tong =" +(x+y));
+        Console.WriteLine("C2");
+        (x, y) = (y, x);
+        Console.WriteLine($"{x} {y}");
+        Console.WriteLine("C3");
+        Console.WriteLine("Tich =" + (x * y));
+        Console.WriteLine("C4");
+       const FttoMeter =
+
+
+    }
+}
