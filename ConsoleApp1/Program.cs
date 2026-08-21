2b262f771a5b5182944a8701b68ab9b41a7a6fc0
phonggioan <tlpgioan210707@gmail.com>
2026-08-21 16:47:25 +0700

    Delete ConsoleApp1 directory

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("em yeu ueh");
-        Console.WriteLine("I am phong");
-    }
-}
